e8b1ba25e6612d56068bce5c806df9dd1c7774e3
Artur Jaakman <jaakmanartur@gmail.com>
2019-09-16 16:37:27 +0300

    Added Renderer

--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { WebGL } from "./WebGL.ts";
+import "./index.css";
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<h1>Loading...</h1>, document.getElementById("root"));
+
+WebGL.onRuntimeInitialized = () => {
+  ReactDOM.render(
+    <canvas
+      className="emscripten"
+      id="canvas"
+      onContextMenu={event => event.preventDefault()}
+      //onClick={WebGL.requestFullScreen(true, false)}
+    ></canvas>,
+    document.getElementById("root")
+  );
+  WebGL.canvas = document.getElementById("canvas");
+};
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
